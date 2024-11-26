dd227c402ecb2206e9b188ecd28c60dca4847021
yaredow <yaredyilma11@gmail.com>
2024-11-26 19:17:39 +0000

    finish the custom user model

--- projects/news/accounts/admin.py
+++ projects/news/accounts/admin.py
@@ -9,6 +9,9 @@ class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
+    list_display = ["email", "username", "age", "is_staff"]
+    fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("age",)}),)  # new
+    add_fieldsets = UserAdmin.add_fieldsets + ((None, {"fields": ("age",)}),)  # new
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
