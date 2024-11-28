4021602cf1025f66760e2b8053c66ae415109ce1
yaredow <yaredyilma11@gmail.com>
2024-11-28 22:35:08 +0000

    add a test for the todo app

--- django-todo-backend/backend/config/settings.py
+++ django-todo-backend/backend/config/settings.py
@@ -37,18 +37,32 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "corsheaders",
     "todos.apps.TodosConfig",
 ]
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.AllowAny",
+    ]
+}
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+CORS_ORIGIN_WHITELIST = (
+    "http://localhost:3000",
+    "http://localhost:8000",
+)
 
 ROOT_URLCONF = "config.urls"
 
--- django-todo-backend/backend/todos/models.py
+++ django-todo-backend/backend/todos/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 
 
-class Todos(models.Model):
-    title: models.CharField(max_length=250)
-    body: models.TextField()
+class Todo(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
 
     def __str__(self):
         return self.title
